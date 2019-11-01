7ea0d5d07ccbe96c4387652bcc97d1e7544cbe8c
Bryce-Soghigian <brybrycodingguy@gmail.com>
2019-11-01 03:19:22 -0500

    finished up intro to oop

--- intro_to_oop.py
+++ intro_to_oop.py
@@ -1,9 +1,9 @@
 """Class
 A class is a blueprint for the object.
-
-We can think of class as an sketch of a parrot with labels. It contains all the details about the name, colors, size etc. Based on these descriptions, we can study about the parrot. Here, parrot is an object.
-
-The example for class of parrot can be :"""
+We can think of class as an sketch of a parrot with labels. 
+It contains all the details about the name, colors, size etc.
+ Based on these descriptions, we can study about the parrot. 
+ Here, parrot is an object."""
 print("=============Parrot Class Prints:================")
 class Parrot:
     #class Attributes
@@ -41,14 +41,9 @@ print(blue.dancing_parrot())
 
 print("=================Use Of Inheritance====================")
 #==========================Use Of Inheritance Python======================++#
-"""
-Inheritance is a way of creating
-new class for using details of
-existing class without modifying it.
-The newly formed class is a derived class
-(or child class). Similarly, the existing
-class is a base class (or parent class).
-"""
+
+print(f"Inheritance is a way of creating\nnew class for using details of\nexisting class without modifying it.\nThe newly formed class is a derived class\n(or child class). Similarly, the existing\nclass is a base class (or parent class).\n")
+
 
 class Bird:
     def __init__(self):
@@ -72,20 +67,16 @@ class Penguin(Bird):
 
     def run(self):
         print("RUN PENGUIN. BECOME BEST BIRD")
-
+print("running tests...")
 penguinNation = Penguin()
 penguinNation.who_am_I()
 penguinNation.swim()
 penguinNation.run()
 
 print("===========Class Encapsulation ==================")
-"""
-Using OOP in Python, we can restrict access
-to methods and variables. This prevent data
-from direct modification which is called encapsulation.
-In Python, we denote private attribute using
- underscore as prefix i.e single “ _ “ or double “ __“.
-"""
+
+print("Using OOP in Python, we can restrict access \n to methods and variables. This prevent data\nfrom direct modification which is called encapsulation.\n In Python, we denote private attribute using\n underscore as prefix i.e single  _  or double  __ .\n")
+
 class Computer:
     def __init__(self):
         self.__maxprice = 900
@@ -97,7 +88,7 @@ class Computer:
         self.__maxprice = price
 
     
-    
+print("running tests...")   
 c= Computer()
 c.sell()
 
@@ -111,7 +102,45 @@ c.sell()
 c.setMaxPrice(1000)
 c.sell()
 
+"""Output"""
+"""Selling Price: 900
+Selling Price: 900
+Selling Price: 1000"""
 #WE have to use a setter function here due to the way we declared
 # our __maxprice attribute.
 
-#Initializing a method that way will 
+#Initializing a method that way will make it private
+
+
+print("============Polymorphism==============")
+print("Polymorphism is an ability (in OOP) to use common\n interface for multiple form (data types).\n Suppose, we need to color a shape,\n there are multiple shape option (rectangle, square, circle).\n However we could use same method to color any shape. \n This concept is called Polymorphism.\n")
+
+class Rhea:
+    def fly(self):
+        print("rhea cannot fly my dude")
+
+    def swim(self):
+        print("Rhea can't swim")
+
+class PolarBear:
+    def fly(self):
+        print("Bruh you think these things can fly?")
+    
+    def swim(self):
+        print("Well they can swim...")
+
+#common interface
+def flying_test(bird):
+    bird.fly()
+
+#instantiate objects
+jamies_bird = Rhea()
+bryces_bear = PolarBear()
+
+
+#passing in the cool objec t
+print("running tests...")
+flying_test(jamies_bird)
+flying_test(bryces_bear)
+    
+    
