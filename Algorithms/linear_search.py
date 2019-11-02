90e7d2668d45a0ed4e36bc30cf8b7894bd0a3310
Bryce-Soghigian <brybrycodingguy@gmail.com>
2019-11-02 10:47:44 -0500

    adding new algo

---
+++ Algorithms/linear_search.py
@@ -0,0 +1,27 @@
+#kw is some keyword 
+#array is some data structure
+"""
+const linear_search = (arr, kw) => {
+    for(let i = 0; i<arr.length; i++){
+        if(i===kw){
+            return kw
+        }else{
+            return "keyword doesn't exist within arr"
+        }
+    }
+} 
+"""
+array = [1,2,4,5,6]
+
+def linear_search(arr,kw):
+    for i in range(0, len(arr)-1):
+        if i == kw:
+            print(kw)
+            return kw
+        else:
+            return f"Keyword doesn't exist in the array"
+
+
+linear_search(array,2)
+#Output === 2
+#this is O(n) is most cases
