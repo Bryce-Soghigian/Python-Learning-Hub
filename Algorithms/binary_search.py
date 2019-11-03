505ea1a66bda1c86494e760b22aff4a6d2c8e5db
Bryce-Soghigian <brybrycodingguy@gmail.com>
2019-11-03 02:42:14 -0600

    added some explanations and recursion

--- Algorithms/binary_search.py
+++ Algorithms/binary_search.py
@@ -17,7 +17,24 @@ while(low<=high){
 
 
 """
+"""
+Explain The interative approach:
+we set up a low and a high
+low = first index aka arr[0]
+high is the length of the arr -1 (arr starts at 0 rather than 1)
+we then set up a while loop that while low is still lower or not equal to high 
+we interate through it. 
+we then declare our middle variable middle = (low+high)/2  //This is just the center of our list
+if keyword < arr[middle]:
+    we use this to find a new high of our list based on the current middle
+ elif keyword > arr[middle]: 
+     remember our while loops condition. We also need to find the new low
+if it gets through those blocks we have found our Keyword
+unless it doesn't exist
 
+
+"""
+#Interative Approach
 def binary_search(arr,keyword):
     low = 0
     high = len(arr) - 1
@@ -32,6 +49,38 @@ def binary_search(arr,keyword):
     return "Not found"
 
 
+def recursive_binary_search(arr, l, r, kw):
+    if r>= 1:
+        
+        middle = 1+ (r-1)/2
+
+        #If Element is present at the middle
+        if arr[middle] == kw:
+            return middle
+        
+        elif arr[middle] > kw:
+            return recursive_binary_search(arr,l, middle-1, kw)
+
+        else:
+            return recursive_binary_search(arr, middle+1, r, kw)
+
+    else:
+        print("not found")
+        return f"Sorry {kw} isn't within this list"
+
+
+"""
+Explain The algorithm:
+    Recursive
+First We compare our keyword with the middle ele
+-if our kw == middle we return the middle index of list
+-elif x is greater than the mid ele then x can only lie in 
+the right half of the subarray
+-Else x is smaller and we recur for the left half
+
+
+"""
+#Runtime
 """
 Even though we have a loop inside our function we get a 
 runtime of O(log(n))
--- Algorithms/insertion_sort.py
+++ Algorithms/insertion_sort.py
@@ -16,6 +16,8 @@ const insertion_sort = (ar) => {
 }
 insertion_sort(a)
 """
+
+
 a = [16, 19, 11, 15, 10, 12, 14]
 
 #iterating over a
@@ -32,3 +34,19 @@ for i in a:
             break
         j = j-1
 print (a)
+
+"""explaining the code
+for i in a: // Here we are iterating through a list
+
+while j>0
+//Index of the current element is positive. This means
+that the element at the index of 'j' is not the first
+element and we need to compare the values
+
+if a[j-1] > a[j] // These Two elements are not in order.
+We need to swap them. 
+
+else // The elements are in order and we can break the loop
+
+a[j-1],a[j] = a[j],a[j-1]//Swapping function
+"""
